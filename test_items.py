0cbad8eefbfff6b1b72c2a0a22c859274bb8d4fc
dean4q <kyrybonis@gmail.com>
2023-07-21 09:14:10 +0300

    ДОВОДКА МЕЛОЧЕЙ

--- test_items.py
+++ test_items.py
@@ -5,9 +5,10 @@ from selenium.webdriver.common.by import By
 link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_button_on_different_language(browser):
+def test_find_button_on_different_language(browser):
 
     browser.get(link)
 
-    assert browser.find_elements(By.CLASS_NAME, "btn-add-to-basket"), 'button not found'
+
+    assert len((browser.find_elements(By.CLASS_NAME, "btn-add-to-basket"), 'button not found')) == 2
     time.sleep(10)
